481012c528c53faea47e43b0dc87e232b9a1d50c
Grunt395 <Grantlew@att.net>
2022-09-07 22:19:16 -0700

    Added Timer functionality

---
+++ assets/js/questions.js
@@ -0,0 +1,17 @@
+var questions = [
+    {
+        title: 'Question 1',
+        choices: ["a", "b", "c", "d"],
+        answers: "a"
+    },
+    {
+        title: 'Question 2',
+        choices: ["a", "b", "c", "d"],
+        answers: "b"
+    },
+    {
+        title: 'Question 3',
+        choices: ["a", "b", "c", "d"],
+        answers: "c"
+    }
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,42 @@
 // alert("Oh baby");
 
+// Global Variables
+var currentQuestionsIndex = 0;
+var time = questions.length * 3;
+var timerID;
+
 // Gets variables from html ID's
 var timeSpanEl = document.getElementById("time");
 var startButton = document.getElementById("start-button");
 var questionsEl = document.getElementById("questions");
 
 
-function StartQuiz() {  
-    // alert("Starting Quiz:");
+function EndQuiz() {
+    clearInterval(timerID);
+
+    var endMenuEl = document.getElementById("end-menu");
+    endMenuEl.removeAttribute("class");
+
+    var finalScoreEl = document.getElementById("final-score");
+    finalScoreEl.textContent = time;
+
+    questionsEl.setAttribute("class", hide);
+}
+
+function CountDown() { // alert("Test");
+    time--;
+    timeSpanEl.textContent = time;
+    if (time <= 0) {
+        EndQuiz();
+    }
+}
+
+function StartQuiz() { // alert("Starting Quiz:");
     var homeScreenEl = document.getElementById("home-menu");
     homeScreenEl.setAttribute("class", "hide");
 
     questionsEl.removeAttribute("class");
-
+    timerID = setInterval(CountDown, 1000);
 
 }
 
