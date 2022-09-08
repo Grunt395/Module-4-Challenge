f0da9cf217733f2b56941795f30bd570d796599d
Grunt395 <Grantlew@att.net>
2022-09-07 21:40:32 -0700

    Added StartQuiz functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
 // alert("Oh baby");
+
+// Gets variables from html ID's
+var timeSpanEl = document.getElementById("time");
+var startButton = document.getElementById("start-button");
+var questionsEl = document.getElementById("questions");
+
+
+function StartQuiz() {  
+    // alert("Starting Quiz:");
+    var homeScreenEl = document.getElementById("home-menu");
+    homeScreenEl.setAttribute("class", "hide");
+
+    questionsEl.removeAttribute("class");
+
+
+}
+
+startButton.addEventListener("click", StartQuiz);
+
+// startButton.onclick = StartQuiz;
