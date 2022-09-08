a381589dd28643a4cfb4d7690e94eea926112ef6
Grunt395 <Grantlew@att.net>
2022-09-07 22:36:12 -0700

    Added question buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@ var timerID;
 var timeSpanEl = document.getElementById("time");
 var startButton = document.getElementById("start-button");
 var questionsEl = document.getElementById("questions");
+var questionChoicesEl = document.getElementById("questions-choices");
 
 
 function EndQuiz() {
@@ -20,7 +21,7 @@ function EndQuiz() {
     var finalScoreEl = document.getElementById("final-score");
     finalScoreEl.textContent = time;
 
-    questionsEl.setAttribute("class", hide);
+    questionsEl.setAttribute("class", "hide");
 }
 
 function CountDown() { // alert("Test");
@@ -31,12 +32,32 @@ function CountDown() { // alert("Test");
     }
 }
 
+function Questions() {}
+
 function StartQuiz() { // alert("Starting Quiz:");
     var homeScreenEl = document.getElementById("home-menu");
     homeScreenEl.setAttribute("class", "hide");
 
     questionsEl.removeAttribute("class");
     timerID = setInterval(CountDown, 1000);
+
+    timeSpanEl.textContent = time;
+
+    var currentQuestion = questions[currentQuestionsIndex];
+    var titleEl = document.getElementsByClassName("questions-title")[0];
+    titleEl.textContent = currentQuestion["title"];
+    questionChoicesEl.innerHTML = "";
+
+    for (let index = 0; index < currentQuestion["choices"].length; index++) {
+        var choice = currentQuestion["choices"][index];
+        var choiceElement = document.createElement('button');
+        choiceElement.setAttribute("class", "choice");
+        choiceElement.setAttribute("value", choice);
+        choiceElement.textContent = `${
+            index + 1
+        }. ${choice}`
+        questionChoicesEl.appendChild(choiceElement);
+    }
 
 }
 
